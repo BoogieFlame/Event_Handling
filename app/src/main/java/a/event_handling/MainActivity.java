ce03c2aa5fd4cb9a2930787152c278d6bb795761
BoogieFlame <kastor.vip@gmail.com>
2020-12-04 23:14:09 +0700

    CommonGestures

--- app/src/main/java/a/event_handling/MainActivity.java
+++ app/src/main/java/a/event_handling/MainActivity.java
@@ -2,7 +2,7 @@ package a.event_handling;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
+import androidx.core.view.GestureDetectorCompat;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,10 +11,15 @@ import android.view.View;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import android.widget.Button;
 import android.widget.TextView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.GestureDetector;
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
+    private TextView gestureText;
+    private GestureDetectorCompat qDetector;
 
     ConstraintLayout myLayout;
 
@@ -25,11 +30,18 @@ public class MainActivity extends AppCompatActivity {
     TextView textView2_lb22;
 
     TextView textView3_lb3;
+
+    boolean lb2_switch = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GestureDetectorCompat gDetector;
         myLayout = (ConstraintLayout) findViewById((R.id.cL));
+        gestureText = findViewById(R.id.tv3_lv);
+        this.qDetector = new GestureDetectorCompat(this,this);
+        qDetector.setOnDoubleTapListener(this);
         textView1_lb1 = (TextView) findViewById(R.id.tv1_lb1);
         button_click_lb1 = (Button) findViewById(R.id.button_lb1);
         button_click_lb1.setOnClickListener(new Button.OnClickListener() {
@@ -50,12 +62,75 @@ public class MainActivity extends AppCompatActivity {
         myLayout.setOnTouchListener(new ConstraintLayout.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent m) {
                 handleTouch(m);
-                return true;
+                return lb2_switch;
             }
         });
 
-        textView3_lb3 = (TextView) findViewById(R.id.tv3_lv2);
+        textView3_lb3 = (TextView) findViewById(R.id.tv3_lv);
+
     }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.qDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        gestureText.setText ("onDown");
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        gestureText.setText("onFling");
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        gestureText.setText("onLongPress");
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                            float distanceX, float distanceY) {
+        gestureText.setText("onScroll");
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        gestureText.setText("onShowPress");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        gestureText.setText("onSingleTapUp");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        gestureText.setText("onDoubleTap");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        gestureText.setText("onDoubleTapEvent");
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        gestureText.setText("onSingleTapConfirmed");
+        return true;
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -75,6 +150,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.lb2:
                 textView2_lb2.setVisibility(View.VISIBLE);
                 textView2_lb22.setVisibility(View.VISIBLE);
+                lb2_switch = true;
                 return true;
             case R.id.lb3:
                 textView3_lb3.setVisibility(View.VISIBLE);
@@ -136,6 +212,7 @@ public class MainActivity extends AppCompatActivity {
 
         textView2_lb2.setVisibility(View.GONE);
         textView2_lb22.setVisibility(View.GONE);
+        lb2_switch = false;
 
         textView3_lb3.setVisibility(View.GONE);
     }
